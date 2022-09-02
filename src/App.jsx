530e1e22fd6d7785bbfa03b2180cda31d289245d
Giovany Inocêncio <ino.giovany@gmail.com>
2022-09-01 23:50:11 -0300

    Estruturando header, sidebar e main

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
+import { Sidebar } from './components/Sidebar';
+
+
+import styles from './App.module.css';
 
 import './global.css';
+
 
 export function App() {
 
   return (
     <>
-      <Header></Header>
-      <Post
-        author="Giovany" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus totam fugiat cum! Eveniet enim, voluptates exercitationem amet quo alias et blanditiis dignissimos nostrum, consequuntur accusamus animi atque tempora debitis aspernatur!"
-      />
+      <Header />
+      
+      <div className={ styles.wrapper }>
+        <Sidebar />
+        <main>
+          <Post author="Giovany" content="Que maneiro"></Post>
+        </main>
+      </div>
+
     </>
+    
     
   )
 }
