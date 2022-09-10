5e19f45d54ad53444b9df98d7090847591c70e90
igiovany <105370259+igiovany@users.noreply.github.com>
2022-09-09 23:14:59 -0300

    Introduzindo iteração

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,40 @@ import { Header } from './components/Header';
 import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 
-
 import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/igiovany.png',
+      name: 'Giovany Inocêncio',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },  
+    ],
+    publishedAt: new Date('2022-09-09 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/guuhschmaltz.png',
+      name: 'Gustavo Schmaltz',
+      role: 'Software Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },  
+    ],
+    publishedAt: new Date('2022-09-10 10:00:00'),
+  },
+];
 
 export function App() {
 
@@ -17,8 +46,15 @@ export function App() {
       <div className={ styles.wrapper }>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
 
