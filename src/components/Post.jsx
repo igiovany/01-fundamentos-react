1e116d7586b9e21cacf1af9b9db21983ab6e80e5
Giovany Inocêncio <ino.giovany@gmail.com>
2022-09-13 21:42:59 -0300

    Validando formulário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -27,15 +27,18 @@ export function Post({ author, publishedAt, content }) {
 
     function handleCreateNewComment() {
         event.preventDefault();
-
         setComments([...comments, newCommentText]);
         setNewCommentText('');
-
     }
 
-    function handleNewCommentChange(){
+    function handleNewCommentChange() {
+        event.target.setCustomValidity('')
         setNewCommentText(event.target.value);
-    };
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório')
+    }
 
     function deleteComment (commentToDelete) {
         // imutabilidade -> as variáveis não sofrem mutação, nós criamos um novo valor (um novo espaço na memória)
@@ -46,6 +49,8 @@ export function Post({ author, publishedAt, content }) {
 
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return (
         <article className= {styles.post}>
@@ -83,9 +88,13 @@ export function Post({ author, publishedAt, content }) {
                     placeholder="Deixe um comentário"
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
